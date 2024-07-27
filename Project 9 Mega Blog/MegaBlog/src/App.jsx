d4f27d0eba250ade44b2d20f8bd58d4cb2ef1643
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-07-27 23:27:04 +0530

    Starting of New Mega Project Bloging App

---
+++ Project 9 Mega Blog/MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log();
+
+  return (
+   <>
+      <h1>Mega Project Blog</h1>
+   </>
+  )
+}
+
+export default App
