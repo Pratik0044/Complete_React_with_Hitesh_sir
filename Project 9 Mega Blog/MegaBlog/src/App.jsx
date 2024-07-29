23e0c46227deece2039b36fac499a3817d3a766d
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-07-29 23:40:47 +0530

    Update in Mega Project

--- Project 9 Mega Blog/MegaBlog/src/App.jsx
+++ Project 9 Mega Blog/MegaBlog/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
 import './App.css'
-import conf from './config/conf';
+import authService from './appwrite/auth'
+import {login, logout} from './store and features/authSlice'
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
 
 function App() {
-  console.log(conf.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData) 
+        dispatch(login({userData}))
+      else
+        dispatch(logout())
+    })
+    .finally(()=> setLoading(false))
+  },[])
 
-  return (
-   <>
-      <h1>Mega Project Blog</h1>
-   </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+  
 }
 
 export default App
