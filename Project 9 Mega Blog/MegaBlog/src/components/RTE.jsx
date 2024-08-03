08b2379d9a0c60cef3c2ab0cf93e9d165d15386b
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-08-03 15:17:30 +0530

    Update in MegaProject RTE.jsx

---
+++ Project 9 Mega Blog/MegaBlog/src/components/RTE.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form"
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+    return (
+        <div className='w-full'>
+            {label && <labe className='inline-block mb-1 pl-1 '>
+                {label}
+            </labe>}
+
+            <Controller
+                name={name || "content"}
+                control={control}
+                render={({ field: { onChange } }) => (
+                    <Editor
+                        initialValue={defaultValue}
+                        init={{
+                            height: 500,
+                            menubar: true,
+                            plugins: [
+                                "image",
+                                "advlist",
+                                "autolink",
+                                "lists",
+                                "link",
+                                "charmap",
+                                "preview",
+                                "anchor",
+                                "searchreplace",
+                                "visualbloacks",
+                                "code",
+                                "fullscreen",
+                                "inserdatatime",
+                                "media",
+                                "table",
+                                "code",
+                                "help",
+                                "wordcount",
+                                "anchor",
+
+                            ],
+                            toolbar: "undo redo | bloacks | image| bold italic forecolor | alignleft aligncenter alignright alignjustify| bullist numlist outdent indent| removeformat | help",
+                            content_style: "body {font-family: Helvetica, Arial, sans-serif; font-size: 14px }"
+                        }}
+                        onEditorChange={onChange}
+                    />
+                )    
+            }
+            />
+        </div>
+
+
+    )
+}
+
